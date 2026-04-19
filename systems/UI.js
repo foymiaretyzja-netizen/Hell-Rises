e4bcb707eff344970a69432799667daa782f0db5
foymiaretyzja-netizen <foymiaretyzja@gmail.com>
2026-04-18 19:58:41 -0500

    Update UI.js

--- systems/UI.js
+++ systems/UI.js
@@ -1,1 +1,10 @@
 
+// systems/UI.js
+
+export function drawUI(ctx) {
+    // We will build this out later!
+    // For now, let's just print a message so you know it's connected.
+    ctx.fillStyle = 'white';
+    ctx.font = '16px Arial';
+    ctx.fillText('UI System Online - Press any button on PS5 controller to connect', 20, 30);
+}
